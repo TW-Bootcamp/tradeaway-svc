56851dd7920edfbd941114a7c9cf412038e4803e
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-01-16 16:14:08 +0530

    Siva, Prateek, Aadarsh | 0 | Setting up flyway db migrations.

--- src/main/resources/db/migration/postgres/V1__init.sql
+++ src/main/resources/db/migration/postgres/V1__init.sql
@@ -1,4 +1,9 @@
-delete from users;
+
+CREATE TABLE users (
+	id serial primary key,
+	login varchar(255) not null,
+	password varchar(255) not null
+);
 
 insert into users(id, login, password) values (1, 'superadmin', 'superadmin123');
 insert into users(id, login, password) values (2, 'admin', 'admin123');
