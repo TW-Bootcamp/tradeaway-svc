8dd9a833eb03663b26e79101892e7285a1b31039
Pooja Dhupar <poojadhupar@pdhupar.local>
2017-01-17 17:34:59 +0530

    1 - Pooja/Swati - Permit /users/register as public api

--- src/main/java/com/tw/tradeaway/config/WebSecurityConfig.java
+++ src/main/java/com/tw/tradeaway/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import com.tw.tradeaway.security.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -64,6 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                 .antMatchers("/auth").permitAll()
+                .antMatchers("/users/register").permitAll()
                 .anyRequest().authenticated();
 
         // Custom JWT based security filter
