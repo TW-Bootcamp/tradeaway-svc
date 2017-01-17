a3749f0b1b96b2e5cb8d93897f8e478c8b2aa04b
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-01-17 16:06:46 +0530

    Siva, Prateek, Aadarsh | 0 | Setting up Spring Security for authentication.

--- src/main/java/com/tw/tradeaway/domain/User.java
+++ src/main/java/com/tw/tradeaway/domain/User.java
@@ -2,13 +2,11 @@ package com.tw.tradeaway.domain;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.tw.tradeaway.LocalDateDeserializer;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
-/**
- * Created by prateeks on 1/13/17.
- */
 @Entity
 @Table(name="users")
 public class User {
@@ -42,10 +40,14 @@ public class User {
 
     @Column(name="dob", nullable = false)
     @JsonDeserialize(using = LocalDateDeserializer.class)
+    @Type(type="java.time.LocalDate")
     private LocalDate dob; //YYYY-MM-DD
 
     @Column(name="email_verified")
     private boolean email_verified;
+
+    @Column(name = "authority")
+    private String authority;
 
     public Long getId() {
         return id;
@@ -55,90 +57,91 @@ public class User {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public void setEmail_verified(boolean email_verified) {
-        this.email_verified = email_verified;
-    }
-
-    public String getType() {
-        return type;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getAddress() {
         return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getMobile() {
         return mobile;
     }
 
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public char getGender() {
         return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
     }
 
     public LocalDate getDob() {
         return dob;
     }
 
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
     public boolean isEmail_verified() {
         return email_verified;
     }
 
+    public void setEmail_verified(boolean email_verified) {
+        this.email_verified = email_verified;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
-
-
-
-
-
