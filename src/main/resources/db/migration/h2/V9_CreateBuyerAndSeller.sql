bbff99ec56156816285eb1c02d748cbef3e34126
ychukka <ychukka@insravanns.local>
2017-01-18 13:08:54 +0530

    adding h2 migration for buyer and seller by yamuna

--- src/main/resources/db/migration/h2/V9_CreateBuyerAndSeller.sql
+++ src/main/resources/db/migration/h2/V9_CreateBuyerAndSeller.sql
@@ -1,13 +1,3 @@
-
-CREATE TABLE buyers (
-id serial primary key,
-gender char(1) default null,
-dob DATE not null,
-user_id INT not null,
-FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-
 CREATE TABLE sellers (
 id serial primary key,
 pan_number varchar(255) not null,
@@ -15,4 +5,10 @@ experience_in_months int not null,
 user_id INT not null,
 FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
+CREATE TABLE buyers (
+id serial primary key,
+gender char(1) default null,
+dob DATE not null,
+user_id INT not null,
+FOREIGN KEY (user_id) REFERENCES users (id)
+);
