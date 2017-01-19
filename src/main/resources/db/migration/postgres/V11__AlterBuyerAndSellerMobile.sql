d828fc26c07b7312ad2e74a364ac04f4af77bd51
ychukka <ychukka@insravanns.local>
2017-01-19 12:03:40 +0530

    alter buyer and seller mobile

--- src/main/resources/db/migration/postgres/V11__AlterBuyerAndSellerMobile.sql
+++ src/main/resources/db/migration/postgres/V11__AlterBuyerAndSellerMobile.sql
@@ -1,3 +1,3 @@
 
-alter table buyers add column mobile  varchar(10) not null;
-alter table sellers add column mobile  varchar(10) not null;
+alter table buyers alter column mobile  varchar(10) not null;
+alter table sellers alter column mobile  varchar(10) not null;
